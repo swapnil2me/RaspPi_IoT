beb0914ead8c750b3e948c0d7b1616f869d9a5f3
OPDTNY <swapnilYoga@users.noreply.github.com>
2021-06-11 16:06:04 +0530

    Threading for sensor operation

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__,
 
 
 alive = 0
-data = {}
+data = {'distance': None}
 
 
 GPIO_TRIGGER = 18
@@ -21,32 +21,35 @@ GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 
-def get_distange():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+def get_distance():
+    global data
+    while True:
+        # set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER, False)
 
-    StartTime = time.time()
-    StopTime = time.time()
-
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
+        # save StartTime
+        while GPIO.input(GPIO_ECHO) == 0:
+            StartTime = time.time()
 
-    return distance
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
+
+        # time difference between start and arrival
+        TimeElapsed = StopTime - StartTime
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
+        data['distance'] = distance
+
+    # return distance
 
 
 @app.route('/')
@@ -76,11 +79,11 @@ def keep_alive_amin():
 
 @app.route('/distance', methods = ['GET','POST'])
 def distance():
-    dist = get_distange()
+    dist = data['distance']
     return jsonify(dist)
 
 
 if __name__ == '__main__':
-    # sensorsThread = threading.Thread(target = get_distange)
-    # sensorsThread.start()
+    sensorsThread = threading.Thread(target = get_distance)
+    sensorsThread.start()
     app.run(host='10.56.241.222', port=8080, debug=True)
